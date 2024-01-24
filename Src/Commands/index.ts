695fa1c93d3b75ce6b29123668d08a0d6b6e2b12
Thenula <149216443+masterthenu1@users.noreply.github.com>
2024-01-24 08:05:37 +0530

    Update index.ts

--- Src/Commands/index.ts
+++ Src/Commands/index.ts
@@ -1,8 +1,14 @@
 import { PREFIX } from '../config';
 import play from './play';
 import help from './help';
+import lyrics from './lyrics';
+import alexa from './alexa';
+
 
 export default {
   [`${PREFIX}play`]: play,
   [`${PREFIX}help`]: help,
+  [`${PREFIX}lyrics`]: lyrics,
+   [`${PREFIX}alexa`]: alexa,
+
 };
