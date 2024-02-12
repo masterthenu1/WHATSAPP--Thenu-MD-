47a84a490faa4c7b0be68568dba15956b32e2d02
Thenula <149216443+masterthenu1@users.noreply.github.com>
2024-02-12 18:29:07 +0530

    Create lyrics.js

---
+++ Src/lyrics.js
@@ -0,0 +1,32 @@
+------  •THENU MD•🧑‍💻
+  |
+  |
+  |
+  |
+
+
+
+import fetch from 'node-fetch'
+
+let handler = async (m, {conn, text }) => {
+  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
+   if (!teks) throw `✳️ Enter the name of the song`
+   try {
+  let res = await fetch(global.API('https://some-random-api.com', '/lyrics', { title: teks }))
+  if (!res.ok) throw await res.text()
+  let json = await res.json()
+  if (!json.thumbnail.genius) throw json
+  conn.sendFile(m.chat, json.thumbnail.genius, null, `
+▢ *${json.title}*
+*${json.author}*\n
+${json.lyrics}`, m)
+m.react(done)
+} catch (e) {
+	m.react(error)
+	} 
+}
+handler.help = ['lyrics']
+handler.tags = ['tools']
+handler.command = ['letra', 'lyrics', 'letras'] 
+
+export default handler
